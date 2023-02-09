36883aa67527221edb3108489b44775adc49eab1
CDGstudent <gearforcoding@gmail.com>
2023-02-09 22:10:56 +0700

    Assignment 5 - connect with MongoDB

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 const { request } = require('express');
 
 //load env vars
-dotenv.config({path:'C:\VacQ\config\config.env'});
+dotenv.config({path: './config/config.env'});
+
+//connect to DB
+connectDB();
 
 const app = express();
+
+//add body parser
+app.use(express.json());
 
 //for hospitals
 const hospitals = require('./routes/hospitals');
 app.use('/api/v1/hospitals',hospitals);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=> {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+});
